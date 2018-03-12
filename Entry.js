39ade2efc123b62871faa5ef77826f703767920b
MantasZvirb <MantasZvirb@gmail.com>
2018-03-12 19:40:47 +0200

    Modified Entry.js

--- Entry.js
+++ Entry.js
@@ -73,6 +73,7 @@ function RandInt(MinValue,MaxValue)
 }
 
 //Entry
+//TEEEEESTING
 //PhysicsObjectCreate([X,Y,Z],Radius,Mass,[SpeedX,SpeedY,SpeedZ],[R,G,B]);
 
 for(let i = 0; i < RandInt(400,500);i++)
